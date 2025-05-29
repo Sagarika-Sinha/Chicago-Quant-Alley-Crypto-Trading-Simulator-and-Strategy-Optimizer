b23861f5ab1267c089c32d488b1a161ca289f197
code-and-calculus <sagarikasinhaa@gmail.com>
2025-05-29 08:27:17 +0530

    Update week1_collect_weekly_data.py

--- week1_collect_weekly_data.py
+++ week1_collect_weekly_data.py
@@ -1,6 +1,19 @@
+
 import requests
 from datetime import datetime,timedelta,timezone
-symbol='BTCUSD'
+import random
+url_for_symbols="https://api.delta.exchange/v2/products"
+symbols_response=requests.get(url_for_symbols)
+if symbols_response.status_code==200:
+    products=symbols_response.json()['result']
+    active_symbols=[p['symbol'] for p in products if 'symbol' in p]
+    if not active_symbols:
+        raise Exception("No active symbols found")
+    symbol=random.choice(active_symbols)
+else:
+    raise Exception("Failed to fetch product list from Delta Exchange.")
+print(f"Collecting data for symbol: {symbol}")
+
 resolution="1h"
 end_time=datetime.now(timezone.utc)
 start_time=end_time-timedelta(days=7)
