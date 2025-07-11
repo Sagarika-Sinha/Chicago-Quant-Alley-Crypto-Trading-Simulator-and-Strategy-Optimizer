878398f8a80e8a88a72cb28c0b0618c3b0a070e5
Sagarika-Sinha <sagarikasinhaa@gmail.com>
2025-07-11 05:10:29 +0000

    updates

--- config.py
+++ config.py
@@ -1,16 +1,9 @@
-# config.py
-
 import os
-
 startDate = "20250701"
-endDate = "20250705"
-
-# Pick one day (preferably the expiry date) to extract available symbols from
+endDate = "20250711"
 expiry_date = "20250704"
 data_folder = os.path.join("data", expiry_date)
-
 symbols = []
-
 if os.path.exists(data_folder):
     for fname in os.listdir(data_folder):
         if fname.startswith("MARK:") and fname.endswith(".csv"):
